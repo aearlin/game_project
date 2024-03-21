ad33e3468c0fa2f2aa6e351164b539d1f42a67ed
Amy Arlin <37715783+aearlin@users.noreply.github.com>
2024-03-21 12:39:29 -0400

    Added environment file and variable for backend port

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000" });
+const backendPort = process.env.REACT_APP_BACKEND_PORT || 4000;
+const API = axios.create({ baseURL: `http://localhost:${backendPort}` });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
