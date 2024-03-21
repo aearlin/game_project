197e95bf64602c144d2fd8e83510f9473481472f
Amy Arlin <37715783+aearlin@users.noreply.github.com>
2024-03-21 12:49:25 -0400

    Changed env file variable from PORT to URL

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const backendPort = process.env.REACT_APP_BACKEND_PORT || 4000;
-const API = axios.create({ baseURL: `http://localhost:${backendPort}` });
+const backendURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:4000";
+const API = axios.create({ baseURL: backendURL });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
