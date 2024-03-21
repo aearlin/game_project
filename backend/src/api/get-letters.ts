a8325c03e0b98d37341590127209b8b5c74de7e4
Amy Arlin <37715783+aearlin@users.noreply.github.com>
2024-03-21 13:14:08 -0400

    Create a random string from a specific set of letters

--- backend/src/api/get-letters.ts
+++ backend/src/api/get-letters.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from 'express';
-
+import { MAX_LETTER_START, MAX_STRING_START, MIN_LETTER } from '../constants/string-constants';
 const getLetters = async (req: Request, res: Response) => {
   try {
     const lettersArray: string[] = [];
-    for (let i = 0; i < 10; i++) {
-      lettersArray.push(String.fromCharCode('a'.charCodeAt(0) + i));
+    for (let i = 0; i < MAX_STRING_START; i++) {
+      lettersArray.push(getRandomLetter(MIN_LETTER, MAX_LETTER_START));
     }
     res.status(200).json({ lettersArray });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
+}
+
+function getRandomLetter(min: number, max: number): string {
+  const randomNum: number = Math.floor(Math.random() * (max - min + 1)) + min;
+  return String.fromCharCode('a'.charCodeAt(0) + randomNum);
 }
 
 export default getLetters;
